f0981b45984c47afec760291a3484d0c8f8c5687
ibrahimaliyevv2 <7cauc3b@code.edu.az>
2022-04-13 16:15:26 +0400

    formtasks added

---
+++ NewDateTimeTask/NewDateTimeTask/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace NewDateTimeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            Console.WriteLine($"Today is {today.Day} of {today.ToString("MMMM")} of {today.Year}. It's {today.ToString("HH:mm:ss")}");
+        }
+    }
+}
